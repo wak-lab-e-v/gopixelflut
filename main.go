10b6726f3d3af58b98e64f3b0107eda7fd40a610
der-den <denny1977@googlemail.com>
2021-12-27 16:01:38 +0100

    Update main.go

--- main.go
+++ main.go
@@ -6,7 +6,9 @@ package main
 import (
 	"bufio"
 	//	"encoding/hex"
+	"errors"
 	"fmt"
+	"image/color"
 	"log"
 	"net"
 	"os"
@@ -28,12 +30,14 @@ const (
 /* global variable declaration */
 var matrix [display_x][display_y]string
 var matrix_rgb [display_x][display_y][3]byte
+var errInvalidFormat = errors.New("invalid hex color format")
 
 func main() {
 
-	// init display
+	//
+	// init display/canvas/matrix
+	//
 	fmt.Printf("Init display with %v x %v\n", display_x, display_y)
-
 	for i := 0; i < display_x; i++ {
 		for j := 0; j < display_y; j++ {
 
@@ -45,7 +49,9 @@ func main() {
 		}
 	}
 
+	//
 	// Start the server and listen for incoming connections.
+	//
 	fmt.Println("Starting " + connType + " server on " + connHost + ":" + connPort)
 	l, err := net.Listen(connType, connHost+":"+connPort)
 	if err != nil {
@@ -74,6 +80,41 @@ func main() {
 	}
 }
 
+func ParseHexColorFast(s string) (c color.RGBA, err error) {
+	c.A = 0xff
+
+	if s[0] != '#' {
+		return c, errInvalidFormat
+	}
+
+	hexToByte := func(b byte) byte {
+		switch {
+		case b >= '0' && b <= '9':
+			return b - '0'
+		case b >= 'a' && b <= 'f':
+			return b - 'a' + 10
+		case b >= 'A' && b <= 'F':
+			return b - 'A' + 10
+		}
+		err = errInvalidFormat
+		return 0
+	}
+
+	switch len(s) {
+	case 7:
+		c.R = hexToByte(s[1])<<4 + hexToByte(s[2])
+		c.G = hexToByte(s[3])<<4 + hexToByte(s[4])
+		c.B = hexToByte(s[5])<<4 + hexToByte(s[6])
+	case 4:
+		c.R = hexToByte(s[1]) * 17
+		c.G = hexToByte(s[2]) * 17
+		c.B = hexToByte(s[3]) * 17
+	default:
+		err = errInvalidFormat
+	}
+	return
+}
+
 // handleConnection handles logic for a single connection request.
 func handleConnection(conn net.Conn) {
 
@@ -88,24 +129,48 @@ func handleConnection(conn net.Conn) {
 		return
 	}
 
+	switch command := string(buffer[0:2]); command {
+	case "IH":
+	case "CC":
+	case "SP":
+	case "GP":
+	case "GM":
+		getMatrix(conn)
+	}
+
 	//
-	// Set Pixel
+	// Send some info on request
 	//
+	if string(buffer[0:1]) == "I" {
+
+	}
+
+	//
+	// Command & Control (CC)
+	//
+	if string(buffer[0:1]) == "CC" {
+
+	}
+
+	//
+	// Set Pixel (SP)
+	//
+	// Sample: 	SP X1 Y1 #FFFFFF
+	//			SP X01 Y33 R0 G255 B17     <-- faster
+	//
+
 	if string(buffer[0:2]) == "SP" {
 
 		xyc := strings.Split(string(buffer[3:]), " ")
 
-		if len(xyc) < 3 {
-			conn.Write([]byte("Too few arguments."))
-			conn.Close()
-			return
+		if debug == true {
+			log.Println("DEBUG: Set Pixel: ", xyc)
 		}
 
-		if debug == true {
-			log.Println("DEBUG: Full IN: ", xyc)
-			log.Println("DEBUG: SP Data X: ", xyc[0])
-			log.Println("DEBUG: SP Data Y: ", xyc[1])
-			log.Println("DEBUG: SP Data C: ", xyc[2])
+		if (len(xyc) < 3) || (len(xyc) > 5) {
+			conn.Write([]byte("Too few or many arguments. Use I command."))
+			conn.Close()
+			return
 		}
 
 		// convert x to int
@@ -168,7 +233,11 @@ func handleConnection(conn net.Conn) {
 		bufferOut = []byte("OK, " + string(buffer[3:]) + "\n")
 	}
 
+	//
 	// Get Pixel
+	// Get color from X/Y
+	// Result as #FFFFF
+	//
 	if string(buffer[0:2]) == "GP" {
 
 		xy := strings.Split(string(buffer[3:]), " ")
@@ -228,30 +297,6 @@ func handleConnection(conn net.Conn) {
 		log.Print("GP from " + conn.RemoteAddr().String())
 	}
 
-	var checksum uint64 = 0
-	var check int = 0
-
-	// Get Matrix
-	if string(buffer[0:2]) == "GM" {
-
-		for j := 0; j < display_y; j++ {
-			for i := 0; i < display_x; i++ {
-				conn.Write([]byte(matrix[i][j]))
-				check, err = strconv.Atoi(matrix[i][j])
-				if err == nil {
-					checksum = checksum + uint64(check)
-				}
-
-			}
-		}
-
-		check_string := strconv.FormatUint(checksum, 10)
-		conn.Write([]byte("\r\n#" + check_string))
-		bufferOut = []byte("\r\n")
-
-		log.Println("GM from " + conn.RemoteAddr().String())
-	}
-
 	// Print response message, stripping newline character.
 	// log.Println("Client message:", string(buffer[:len(buffer)-1]))
 
@@ -260,4 +305,36 @@ func handleConnection(conn net.Conn) {
 
 	// Restart the process.
 	handleConnection(conn)
+}
+
+func infoAndHelp() {
+
+}
+
+//
+// Get Matrix
+// Send full matrix row 1, col 1 to X, row 2, col 1 to X and so on
+// Result as stream of ASCII endoded Hex (RGB)
+//
+func getMatrix(conn net.Conn) {
+	var checksum uint64 = 0
+
+	for j := 0; j < display_y; j++ {
+		for i := 0; i < display_x; i++ {
+			conn.Write([]byte(matrix[i][j]))
+
+			check, err := strconv.Atoi(matrix[i][j]) // build checksum
+			if err == nil {
+				checksum = checksum + uint64(check)
+			}
+
+		}
+	}
+
+	check_string := strconv.FormatUint(checksum, 10)
+	conn.Write([]byte("\r\n#" + check_string))
+	conn.Write([]byte("\r\n"))
+
+	log.Println("GM from " + conn.RemoteAddr().String())
+
 }
