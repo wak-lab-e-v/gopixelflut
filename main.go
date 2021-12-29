d364bce01ac151ea69696ce5d826290ae45e913a
Martin <mhilde@gmx.de>
2021-12-29 15:58:25 +0100

    Extra Thread

--- main.go
+++ main.go
@@ -3,24 +3,24 @@
 package main
 
 import (
-        "bufio"
-        "fmt"
-        "log"
-        "net"
-        "os"
-        "strconv"
-        "strings"
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strconv"
+	"strings"
 )
 
 // Application constants, defining host, port, and protocol.
 const (
-        connHost  = "0.0.0.0"
-        connPort  = "1337"
-        connType  = "tcp"
-        display_x = 60
-        display_y = 33
-        valueMax  = 7
-        debug     = false
+	connHost  = "0.0.0.0"
+	connPort  = "1337"
+	connType  = "tcp"
+	display_x = 60
+	display_y = 33
+	valueMax  = 7
+	debug     = false
 )
 
 /* global variable declaration */
@@ -28,231 +28,216 @@ var matrix [display_x][display_y]string
 
 func main() {
 
-        // init display
-        fmt.Printf("Init display with %v x %v\n", display_x, display_y)
+	// init display
+	fmt.Printf("Init display with %v x %v\n", display_x, display_y)
 
-        for i := 0; i < display_x; i++ {
-                for j := 0; j < display_y; j++ {
+	for i := 0; i < display_x; i++ {
+		for j := 0; j < display_y; j++ {
 
-                        matrix[i][j] = "000000"
-                }
-        }
+			matrix[i][j] = "000000"
+		}
+	}
 
-        // Start the server and listen for incoming connections.
-        fmt.Println("Starting " + connType + " server on " + connHost + ":" + connPort)
-        l, err := net.Listen(connType, connHost+":"+connPort)
-        if err != nil {
-                fmt.Println("Error listening:", err.Error())
-                os.Exit(1)
-        }
-        // Close the listener when the application closes.
-        defer l.Close()
+	// Start the server and listen for incoming connections.
+	fmt.Println("Starting " + connType + " server on " + connHost + ":" + connPort)
+	l, err := net.Listen(connType, connHost+":"+connPort)
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		os.Exit(1)
+	}
+	// Close the listener when the application closes.
+	defer l.Close()
 
-        // run loop forever, until exit.
-        for {
-                // Listen for an incoming connection.
-                c, err := l.Accept()
-                if err != nil {
-                        fmt.Println("Error connecting:", err.Error())
-                        return
-                }
+	// run loop forever, until exit.
+	for {
+		// Listen for an incoming connection.
+		c, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error connecting:", err.Error())
+			return
+		}
 
-                fmt.Println("Client connected.")
+		fmt.Println("Client connected.")
 
-                // Print client connection address.
-                fmt.Println("Client " + c.RemoteAddr().String() + " connected.")
+		// Print client connection address.
+		fmt.Println("Client " + c.RemoteAddr().String() + " connected.")
 
-                // Handle connections concurrently in a new goroutine.
-                go handleConnection(c)
-        }
+		// Handle connections concurrently in a new goroutine.
+		go handleConnection(c)
+	}
 }
 
 // handleConnection handles logic for a single connection request.
 func handleConnection(conn net.Conn) {
-  //buffer := make([]byte, 1024)
+	//buffer := make([]byte, 1024)
 
-  for {
-        bufferOut := []byte("unkown command \n")
+	for {
+		// Buffer client input until a newline.
+		buffer, err := bufio.NewReader(conn).ReadBytes('\n')
 
-        // Buffer client input until a newline.
-        buffer, err := bufio.NewReader(conn).ReadBytes('\n')
-        //bufLen , err := conn.Read(buffer)
-        //bufferstring := string(buffer)
-        //log.Println(bufLen)
-        //log.Println(bufferstring)
-        //buffersplit := strings.Split(string(buffer), "\n")
-        //bufferstring = buffersplit[0]
-        //log.Println(bufferstring)
-        bufferstring := string(buffer)
-        log.Println(bufferstring)
-        // Close left clients.
-        if err != nil {
-                conn.Close()
-                return
-        }
+		//bufLen , err := conn.Read(buffer)
+		//bufferstring := string(buffer)
+		//log.Println(bufLen)
+		//log.Println(bufferstring)
+		//buffersplit := strings.Split(string(buffer), "\n")
+		//bufferstring = buffersplit[0]
+		//log.Println(bufferstring)
+		//bufferstring := string(buffer)
+		// log.Println(bufferstring)
+		// Close left clients.
+		if err != nil {
+			conn.Close()
+			return
+		}
+		go handleCommand(buffer, conn)
+	}
+}
 
-        if len(bufferstring) < 2 {
-          continue
-        }
+func handleCommand(Command []byte, conn net.Conn) {
+	log.Println(string(Command))
 
-        CMD := bufferstring[0:2]
-        ARG := bufferstring[3:]
-        //
-        // Set Pixel
-        //
-        if CMD == "SP" {
+	if len(Command) < 3 {
+		return
+	}
 
-                xyc := strings.Split(ARG, " ")
+	CmdString := string(Command)
+	CMD := CmdString[0:2]
+	ARG := CmdString[3:]
 
-                if len(xyc) < 3 {
-                        conn.Write([]byte("Too few arguments."))
-                        conn.Close()
-                        return
-                }
+	switch CMD {
+	case "SP":
+		xyc := strings.Split(ARG, " ")
+		if len(xyc) < 3 {
+			conn.Write([]byte("Too few arguments."))
+			return
+		}
+		if debug == true {
+			log.Println("DEBUG: Full IN: ", xyc)
+			log.Println("DEBUG: SP Data X: ", xyc[0])
+			log.Println("DEBUG: SP Data Y: ", xyc[1])
+			log.Println("DEBUG: SP Data C: ", xyc[2])
 
-                if debug == true {
-                        log.Println("DEBUG: Full IN: ", xyc)
-                        log.Println("DEBUG: SP Data X: ", xyc[0])
-                        log.Println("DEBUG: SP Data Y: ", xyc[1])
-                        log.Println("DEBUG: SP Data C: ", xyc[2])
-                }
+		}
+		// convert x to int
+		xInt, err := strconv.Atoi(xyc[0])
 
-                // convert x to int
-                xInt, err := strconv.Atoi(xyc[0])
+		if err != nil {
+			conn.Write([]byte("Error in X."))
+			return
+		}
 
-                if err != nil {
-                        conn.Write([]byte("Error in X."))
-                        conn.Close()
-                        return
-                }
+		if xInt > display_x {
+			conn.Write([]byte("X to big."))
+			return
+		}
 
-                if xInt > display_x {
-                        conn.Write([]byte("X to big."))
-                        conn.Close()
-                        return
-                }
+		if xInt == 0 {
+			conn.Write([]byte("X to small."))
+			return
+		}
 
-                if xInt == 0 {
-                        conn.Write([]byte("X to small."))
-                        conn.Close()
-                        return
-                }
+		// convert y to int
+		yInt, err := strconv.Atoi(xyc[1])
 
-                // convert y to int
-                yInt, err := strconv.Atoi(xyc[1])
+		if err != nil {
+			conn.Write([]byte("Error in Y."))
+			return
+		}
 
-                if err != nil {
-                        conn.Write([]byte("Error in Y."))
-                        conn.Close()
-                        return
-                }
+		if yInt > display_y {
+			conn.Write([]byte("Y to big."))
+			return
+		}
 
-                if yInt > display_y {
-                        conn.Write([]byte("Y to big."))
-                        conn.Close()
-                        return
-                }
+		if yInt == 0 {
+			conn.Write([]byte("Y to small."))
+			return
+		}
 
-                if yInt == 0 {
-                        conn.Write([]byte("Y to small."))
-                        conn.Close()
-                        return
-                }
+		xyc[2] = strings.TrimRight(xyc[2], "\r\n")
 
-                xyc[2] = strings.TrimRight(xyc[2], "\r\n")
+		if len(xyc[2]) != 7 {
+			conn.Write([]byte("Value size missmatch."))
+			return
+		}
 
-                if len(xyc[2]) != 7 {
-                        conn.Write([]byte("Value size missmatch."))
-                        conn.Close()
-                        return
-                }
+		// set 3. value to display matrix
+		matrix[xInt-1][yInt-1] = xyc[2][1:7]
+		//              log.Println("SP from " + xyc[0] + "x" + xyc[1] + " to " + xyc[2] + " from " + conn.RemoteAddr().String())
 
-                // set 3. value to display matrix
-                matrix[xInt-1][yInt-1] = xyc[2][1:7]
-//              log.Println("SP from " + xyc[0] + "x" + xyc[1] + " to " + xyc[2] + " from " + conn.RemoteAddr().String())
+		conn.Write([]byte("OK\n"))
+		return
 
-                bufferOut = []byte("OK\n")
-        }
+	case "GP":
+		// Get Pixel
+		xy := strings.Split(ARG, " ")
 
-        // Get Pixel
-        if CMD == "GP" {
+		if len(xy) < 2 {
+			conn.Write([]byte("Too few arguments."))
+			return
+		}
 
-                xy := strings.Split(ARG, " ")
+		// convert x to int
+		xInt, err := strconv.Atoi(xy[0])
 
-                if len(xy) < 2 {
-                        conn.Write([]byte("Too few arguments."))
-                        conn.Close()
-                        return
-                }
+		if err != nil {
+			conn.Write([]byte("Error in X."))
+			return
+		}
 
-                // convert x to int
-                xInt, err := strconv.Atoi(xy[0])
+		if xInt > display_x {
+			conn.Write([]byte("X to big."))
+			return
+		}
 
-                if err != nil {
-                        conn.Write([]byte("Error in X."))
-                        conn.Close()
-                        return
-                }
+		if xInt == 0 {
+			conn.Write([]byte("X to small."))
+			return
+		}
 
-                if xInt > display_x {
-                        conn.Write([]byte("X to big."))
-                        conn.Close()
-                        return
-                }
+		xy[1] = strings.TrimRight(xy[1], "\r\n")
+		// convert y to int
+		yInt, err := strconv.Atoi(xy[1])
 
-                if xInt == 0 {
-                        conn.Write([]byte("X to small."))
-                        conn.Close()
-                        return
-                }
+		if err != nil {
+			e := fmt.Errorf("%v", err)
+			conn.Write([]byte("Error in Y." + string(e.Error())))
+			return
+		}
 
-                xy[1] = strings.TrimRight(xy[1], "\r\n")
-                // convert y to int
-                yInt, err := strconv.Atoi(xy[1])
+		if yInt > display_y {
+			conn.Write([]byte("Y to big."))
+			return
+		}
 
-                if err != nil {
-                        e := fmt.Errorf("%v", err)
-                        conn.Write([]byte("Error in Y." + string(e.Error())))
-                        conn.Close()
-                        return
-                }
+		if yInt == 0 {
+			conn.Write([]byte("Y to small."))
+			return
+		}
 
-                if yInt > display_y {
-                        conn.Write([]byte("Y to big."))
-                        conn.Close()
-                        return
-                }
+		conn.Write([]byte("#" + matrix[xInt-1][yInt-1] + "\r\n"))
 
-                if yInt == 0 {
-                        conn.Write([]byte("Y to small."))
-                        conn.Close()
-                        return
-                }
+		log.Print("GP from " + conn.RemoteAddr().String())
+	case "GM":
+		// Get Matrix
+		for j := 0; j < display_y; j++ {
+			for i := 0; i < display_x; i++ {
+				conn.Write([]byte(matrix[i][j]))
+			}
+		}
 
-                bufferOut = []byte("#" + matrix[xInt-1][yInt-1] + "\r\n")
+		conn.Write([]byte("\r\n"))
+		log.Println("GM from " + conn.RemoteAddr().String())
+		return
 
-                log.Print("GP from " + conn.RemoteAddr().String())
-        }
+	default:
+		conn.Write([]byte("unkown command \n"))
+		return
+	}
 
-        // Get Matrix
-        if CMD == "GM" {
+	// Print response message, stripping newline character.
+	// log.Println("Client message:", string(buffer[:len(buffer)-1]))
 
-                for j := 0; j < display_y; j++ {
-                        for i := 0; i < display_x; i++ {
-                                conn.Write([]byte(matrix[i][j]))
-                        }
-                }
+	// Send response message to the client.
 
-                bufferOut = []byte("\r\n")
-                log.Println("GM from " + conn.RemoteAddr().String())
-        }
-
-        // Print response message, stripping newline character.
-        // log.Println("Client message:", string(buffer[:len(buffer)-1]))
-
-        // Send response message to the client.
-        conn.Write(bufferOut)
-  }
-        // Restart the process.
-        //handleConnection(conn)
 }
